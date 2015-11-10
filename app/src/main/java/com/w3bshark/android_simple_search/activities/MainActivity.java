911e5d9d8aba748fd332c402790d624fe03f20ff
Tyler McCraw <mccraw.tyler@gmail.com>
2015-11-10 17:03:05 -0500

    Fixed missed super call on onNewIntent in MainActivity

--- app/src/main/java/com/w3bshark/android_simple_search/activities/MainActivity.java
+++ app/src/main/java/com/w3bshark/android_simple_search/activities/MainActivity.java
@@ -143,6 +143,7 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
         handleIntent(intent);
     }
 
